bfad8902f2eb9b45532730876249f8417a1a1902
lyyrhf <55946117+lyyrhf@users.noreply.github.com>
2020-09-18 09:20:01 +0800

    Add files via upload

---
+++ src/main/java/com/kuang/pojo/Books.java
@@ -0,0 +1,18 @@
+package com.kuang.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.Alias;
+
+@Alias("Books")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Books {
+    private int bookID;
+    private String bookName;
+    private int bookCounts;
+    private String detail;
+
+}
